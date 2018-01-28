c33179ebc12f408adb2b74409d65707a660c1a22
karushi <karuship@gmail.com>
2018-01-28 23:30:06 +0300

    add API KEY

--- app/config.py
+++ app/config.py
@@ -2,8 +2,9 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
 
+    NEWS_API_BASE_URL = https://newsapi.org/v2/sources?category={}&apiKey={}
+    
 
 class ProdConfig(Config):
 
