9dc459854ac5f5d35a1a64970eb265ae395c6271
karushi <karuship@gmail.com>
2018-01-29 10:26:15 +0300

    add sources

--- app/request.py
+++ app/request.py
@@ -11,4 +11,18 @@ api_key = app.config['45e91ce609c347dfa903f822aac08388']
 
 # Getting the news base url
 base_url = app.conifg['https://newsapi.org/v2/sources?category={}&apiKey={}']
-def get_news(category)
+
+
+def get_sources(category):
+
+    '''
+    Function that gets the json response to our url request
+    '''
+
+    get_sources_url = base_url.format(category, api_key)
+    with urllib. request.urlopen(get_source_url)as url:
+    get_sources_responce = json.loads(get_sources_data)
+
+    sources_results = None
+if get_sources_responce['results']:
+    sources_results_list = get_sources_responce['results']
