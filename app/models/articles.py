b3fdb85588102db13a53bde6ebb94e69c794b98d
karushi <karuship@gmail.com>
2018-01-30 20:09:26 +0300

    add changes

--- app/models/articles.py
+++ app/models/articles.py
@@ -2,14 +2,14 @@ class Article:
     '''
     news class to define News object
     '''
-    # def __init__(self, id, name, author, title, description, url, urlToImage,
-    #              publishedAt):
-    #
-    #     self.id = id
-    #     self.name = name
-    #     self.author = author
-    #     self.title = title
-    #     self.description = description
-    #     self.url = url
-    #     self.urlToImage = urlToImage
-    #     self.publishedAt = publishedAt
+    def __init__(self, id, name, author, title, description, url, urlToImage,
+                 publishedAt):
+
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
