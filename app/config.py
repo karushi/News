6dc5236b26bdb79e98f1706852e949738f2c8bd9
karushi <karuship@gmail.com>
2018-01-30 20:35:33 +0300

    add changes

--- app/config.py
+++ app/config.py
@@ -7,23 +7,23 @@ class Config:
     ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
 
-# class ProdConfig(Config):
-#
-#     '''
-#     Production  configuration child class
-#
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     pass
-#
-#
-# class DevConfig(Config):
-#     '''
-#     Development  configuration child class
-#
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#
-#     DEBUG = True
+class ProdConfig(Config):
+
+    '''
+    Production  configuration child class
+
+   Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
